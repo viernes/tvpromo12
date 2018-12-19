f16026302ae75426f94f9095140a594ab32ea221
viernes <luis.muro@xmarts.com>
2018-12-19 10:58:34 -0600

    Add files via upload
    Modulo TVP Tema rojo

---
+++ tvp_theme/__manifest__.py
@@ -0,0 +1,31 @@
+
+{
+    "name": "GTVP Theme",
+    "summary": "Odoo 12.0 Enterprise backend theme",
+    "version": "12.0.1.1.6",
+    "category": "Themes/Backend",
+    "license": "AGPL-3",
+    "author": "Xmarts",
+    "website": "www.Xmarts.com",
+    "contributors": [
+        "William Colin Macedo <william.colin@xmarts.com>",
+    ],
+        "depends": [
+    ],
+    "data": [
+        'views/webclient_templates.xml',
+    ],
+    "qweb": [
+        "views/base.xml",
+
+    ],
+    "images": [
+
+    ],
+    "external_dependencies": {
+
+    },
+    "application": False,
+    "installable": True,
+    "auto_install": False,
+}
